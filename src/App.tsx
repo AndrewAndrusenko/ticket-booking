400724fc86f8428a507af16ded7e8c3a55d6937b
Andrey Andrusenko <aandrusenko@yandex.ru>
2025-04-13 21:39:04 +0300

    Styling

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,6 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import pop from '/popcorn.svg'
+import newAgeLogo from './assets/images.png'
 import './App.css'
 
 function App() {
@@ -9,24 +9,20 @@ function App() {
   return (
     <>
       <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
+        <a href="https://andrewandrusenko.github.io/" target="_blank">
+          <img src={pop} className="logo" alt="Vite logo" />
+          <img src={newAgeLogo} className="logo" alt="Vite logo" />
+
         </a>
       </div>
-      <h1>Vite + React</h1>
+      <h1>New Age Cinema</h1>
       <div className="card">
         <button onClick={() => setCount((count) => count + 2)}>
           count is {count}
         </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
       </div>
       <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
+        Click on the New Age logo to learn more
       </p>
     </>
   )
